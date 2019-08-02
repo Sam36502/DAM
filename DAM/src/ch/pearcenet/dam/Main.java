3bfaaa1c6a81094e8b88ff7b82646798f45f8d60
sam <sam@aepearce.com>
2019-08-02 14:16:05 +0200

    Added VM functionality and Console argument handling

--- DAM/src/ch/pearcenet/dam/Main.java
+++ DAM/src/ch/pearcenet/dam/Main.java
@@ -2,11 +2,97 @@ package ch.pearcenet.dam;
 
 public class Main {
 	
+	public static boolean logsEnabled = false;
+	public static boolean keepFiles = false;
+	
 	public static void main(String[] args) {
 		
-		Tag newTag = new Tag();
-		System.out.println("Generated tag: " + newTag.getTag());
+		//Variables for the VM and Language
+		Tag mainTag = null;
+		String stdinString = "";
 		
+		//Help menu
+		if (argumentIndex(args, "help") != -1) {
+			helpMenu();
+			System.exit(0);
+		}
+		
+		//Check the number of arguments is correct
+		if (args.length < 1) {
+			System.out.println("Too few arguments.\n");
+			helpMenu();
+			System.exit(0);
+		}
+		
+		//Set a custom tag to be used
+		if (argumentIndex(args, "tag") != -1) {
+			int tIndex = argumentIndex(args, "tag");
+			mainTag = new Tag(args[tIndex + 1]);
+		}
+		
+		//Set a custom tag to be used
+		if (argumentIndex(args, "in") != -1) {
+			int iIndex = argumentIndex(args, "in");
+			stdinString = args[iIndex + 1];
+		}
+		
+		//Set all logs to be displayed
+		if (argumentIndex(args, "log") != -1) {
+			logsEnabled = true;
+		}
+		
+		//Set the temp files to not be deleted
+		if (argumentIndex(args, "save") != -1) {
+			keepFiles = true;
+		}
+		
+		
+		//Load and run a file with the vm
+		if (mainTag == null) mainTag = new Tag();
+		RandVM vm = new RandVM(mainTag, stdinString);
+		
+		//TODO: Add transpiler and compiler steps here
+		
+		String programFile = args[args.length - 1];
+		
+		
+	}
+	
+	//Logs to the screen as long as logging is enabled
+	public static void log(String msg) {
+		if (logsEnabled) {
+			System.out.println(msg);
+		}
+	}
+	
+	//Checks if a String array contains a value
+	public static int argumentIndex(String[] arr, String val) {
+		for (int i=0; i<arr.length; i++) {
+			if (("--" + val).equals(arr[i]) ||
+				("-" + val.charAt(0)).equals(arr[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	//Help Menu
+	public static void helpMenu() {
+		System.out.println("Usage:\n" + 
+				"  DAM.bat [-ht] program\n" + 
+				"\n" + 
+				"Options:\n" + 
+				"  -h, --help      Display the help menu\n" + 
+				"  -t, --tag tag   Run a program with a specific tag\n" + 
+				"  -l, --log       Runs program and shows info about the process.\n" + 
+				"  -i, --in input  Starts the program with input\n" +
+				"  -s, --save      Saves the temporary files from transpiling\n" + 
+				"                  and compiling, for debugging.\n" + 
+				"\n" + 
+				"Examples:\n" + 
+				"  DAM.bat -h\n" + 
+				"  DAM.bat helloworld.dam\n" + 
+				"  DAM.bat -t ea9140b67f3c2d858bdd3d88595239a8 helloworld.dam");
 	}
 
 }
--- DAM/src/ch/pearcenet/dam/RandVM.java
+++ DAM/src/ch/pearcenet/dam/RandVM.java
@@ -1,16 +1,32 @@
 package ch.pearcenet.dam;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class RandVM {
 	
+	//Maximum number of lines a program can be
+	private static final int MAX_PROGLEN = 256;
+	
 	private Tag tag;
+	private String stdin;
 	private int[] instructionSet = new int[8];
 	private byte[] RAM = new byte[256];
+	private int[][] progMem = new int[MAX_PROGLEN][4];
 	
 	private boolean isIfStatement = false;
 	private boolean isIfSuccessful = false;
 	
-	public RandVM (Tag tag) {
+	private int programCounter = 1;
+	private int inputIndex = 0;
+	
+	//Stores the number of arguments for each instruction
+	private static final int[] argCount = {1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 1, 0, 0, 1, 0};
+	
+	public RandVM (Tag tag, String stdin) {
 		this.tag = tag;
+		this.stdin = stdin;
 		
 		//Generate InstructionSet
 		for (int i=0; i<8; i++) {
@@ -19,50 +35,158 @@ public class RandVM {
 		}
 	}
 	
+	//Loads a binary executable file
+	public void loadBinary(String filename) {
+		Scanner in = null;
+		try {
+			FileInputStream fis = new FileInputStream(filename);
+			in = new Scanner(fis);
+		} catch (FileNotFoundException e) {
+			System.out.println("[ERROR] Couldn't load Binary file '"+filename+"'");
+			return;
+		}
+		
+		//Load all the instructions into program memory
+		int progCount = 1;
+		while (in.hasNext() && progCount < MAX_PROGLEN) {
+			byte insByte = in.nextByte();
+			progMem[progCount][0] = getLogicalIns(insByte);
+			
+			for (int i=0; i<argCount[progMem[progCount][0]]; i++) {
+				progMem[progCount][i + 1] = in.nextByte();
+			}
+		}
+		
+		in.close();
+		
+	}
+	
+	//Run a full program from Prog Memory
+	public void runProg() {
+		Main.log("Running program...\n----");
+		
+		int[] instruction = {0};
+		while (programCounter < MAX_PROGLEN) {
+			instruction = progMem[programCounter++];
+			compute(instruction);
+		}
+		
+		Main.log("----\nProgram Ended.");
+	}
+	
 	//Perform a computation using the VM
-	public void compute(int ins, int... args) {
+	private void compute(int[] instruction) {
+		int ins = instruction[0];
 		
 		//Check if we're in an unsuccessful if block
 		if (isIfStatement && !isIfSuccessful) {
 			return;
 		}
 		
-		//TODO: Check number of args is correct
+		//Check number of args is correct
+		if (argCount[ins] != instruction.length - 1) {
+			Main.log("Incorrect number of ");
+			return;
+		}
 		
 		switch(ins) {
+		
+			//Do Nothing
+			case 16:
+				return;
 			
-			//Get a char from stdin and store in addr //PONDER
-			case 1:
+			//Get a char from stdin and store in addr
+			case 0:
+				RAM[instruction[1]] = (byte) stdin.charAt(inputIndex);
 				break;
 			
 			//Print a char to stdout from addr
-			case 2:
+			case 1:
+				System.out.print("" + (char) RAM[instruction[1]]);
 				break;
 			
 			//Sets an address to a value
+			case 2:
+				RAM[instruction[1]] = (byte) instruction[2];
+				break;
+			
+			//Copies a value from one address to another
 			case 3:
+				RAM[instruction[2]] = RAM[instruction[1]];
 				break;
 			
 			//Adds two numbers and stores result
 			case 4:
+				RAM[instruction[3]] = (byte) (RAM[instruction[1]] + RAM[instruction[2]]);
 				break;
 			
 			//Subs two numbers and stores result
 			case 5:
+				RAM[instruction[3]] = (byte) (RAM[instruction[1]] - RAM[instruction[2]]);
+				break;
+			
+			//Multiplies two numbers and stores result
+			case 6:
+				RAM[instruction[3]] = (byte) (RAM[instruction[1]] * RAM[instruction[2]]);
+				break;
+				
+			//Divides two numbers and stores result
+			case 7:
+				RAM[instruction[3]] = (byte) Math.floorDiv(RAM[instruction[1]],  RAM[instruction[2]]);
+				break;
+				
+			//Divides two numbers and stores remainder
+			case 8:
+				RAM[instruction[3]] = (byte) (RAM[instruction[1]] % RAM[instruction[2]]);
+				break;
+			
+			//Compares if two numbers are equal and stores boolean
+			case 9:
+				if (RAM[instruction[1]] == RAM[instruction[2]]) {
+					RAM[instruction[3]] = 1;
+				} else {
+					RAM[instruction[3]] = 2;
+				}
+				break;
+			
+			//Compares if one number is greater than the other and stores boolean
+			case 10:
+				if (RAM[instruction[1]] > RAM[instruction[2]]) {
+					RAM[instruction[3]] = 1;
+				} else {
+					RAM[instruction[3]] = 2;
+				}
+				break;
+			
+			//Compares if one number is less than the other and stores boolean
+			case 11:
+				if (RAM[instruction[1]] < RAM[instruction[2]]) {
+					RAM[instruction[3]] = 1;
+				} else {
+					RAM[instruction[3]] = 2;
+				}
 				break;
 			
 			//Start if statement
-			case 6:
+			case 12:
+				isIfStatement = true;
+				isIfSuccessful = (RAM[instruction[1]] == 1);
 				break;
 			
 			//end if, start else
-			case 7:
+			case 13:
+				isIfSuccessful = !isIfSuccessful;
 				break;
 			
 			//end if/else statement
-			case 8:
+			case 14:
+				isIfStatement = false;
 				break;
 			
+			//Jumps to a specific line in program memory
+			case 15:
+				programCounter = RAM[instruction[1]];
+				break;
 		}
 		
 	}
