1efe45bc3e78fe0f19f681b94207ea00596fe90d
Samuel Pearce <sam@aepearce.com>
2019-08-08 08:17:55 +0200

    Updated Main

--- DAM/src/ch/pearcenet/dam/Main.java
+++ DAM/src/ch/pearcenet/dam/Main.java
@@ -147,8 +147,8 @@ public class Main {
 				"Examples:\n" + 
 				"  DAM.bat -h\n" + 
 				"  DAM.bat helloworld.dam\n" + 
-				"  DAM.bat -t 674daf093e8b51c2948f5bb14b426c3d helloworld.dam\n" +
-				"  DAM.bat --log --show-prog --save-tmp --in Hello helloworld.dam");
+				"  DAM.bat -t 674daf093e8b51c2948f5bb14b426c3d progs/helloworld.dam\n" +
+				"  DAM.bat --log --show-prog --save-tmp --in Hello progs/cat.dam");
 	}
 
 }
